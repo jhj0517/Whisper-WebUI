ebb9b012bd5485c79d482e46f7ac513c474e056a
jhj0517 <97279763+jhj0517@users.noreply.github.com>
2023-09-11 22:11:30 +0900

    refactoring

--- modules/youtube_manager.py
+++ modules/youtube_manager.py
@@ -1,12 +1,15 @@
 from pytube import YouTube
 import os
 
+
 def get_ytdata(link):
     return YouTube(link)
 
+
 def get_ytmetas(link):
     yt = YouTube(link)
-    return yt.thumbnail_url,yt.title,yt.description
+    return yt.thumbnail_url, yt.title, yt.description
 
-def get_ytaudio(ytdata:YouTube):
+
+def get_ytaudio(ytdata: YouTube):
     return ytdata.streams.get_audio_only().download(filename=os.path.join("modules", "yt_tmp.wav"))
