5a1d3d20fdd9f68920881cd484779c901ee7aac7
jhj0517 <97279763+jhj0517@users.noreply.github.com>
2024-01-22 16:55:13 +0900

    fix bug

--- app.py
+++ app.py
@@ -33,9 +33,9 @@ class App:
     def on_change_models(model_size: str):
         translatable_model = ["large", "large-v1", "large-v2", "large-v3"]
         if model_size not in translatable_model:
-            return gr.Checkbox.update(visible=False, value=False, interactive=False)
+            return gr.Checkbox(visible=False, value=False, interactive=False)
         else:
-            return gr.Checkbox.update(visible=True, value=False, label="Translate to English?", interactive=True)
+            return gr.Checkbox(visible=True, value=False, label="Translate to English?", interactive=True)
 
     def launch(self):
         with self.app:
