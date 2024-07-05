0531958a0854c3a4271deb3d8bbc815c653987ce
jhj0517 <97279763+jhj0517@users.noreply.github.com>
2024-07-05 16:52:41 +0900

    Fix `get_device()``

--- modules/whisper/faster_whisper_inference.py
+++ modules/whisper/faster_whisper_inference.py
@@ -145,7 +145,5 @@ class FasterWhisperInference(WhisperBase):
     def get_device():
         if torch.cuda.is_available():
             return "cuda"
-        elif torch.backends.mps.is_available():
-            return "auto"
         else:
-            return "cpu"
+            return "auto"
