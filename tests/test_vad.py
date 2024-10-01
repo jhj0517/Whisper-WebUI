dc53364225f711ccd7d67e24ba44a6073dd1945b
jhj0517 <97279763+jhj0517@users.noreply.github.com>
2024-10-01 21:37:36 +0900

    Add VAD test

---
+++ tests/test_vad.py
@@ -0,0 +1,25 @@
+from modules.utils.paths import *
+from modules.whisper.whisper_factory import WhisperFactory
+from modules.whisper.whisper_parameter import WhisperValues
+from test_config import *
+from test_transcription import download_file, test_transcribe
+
+import gradio as gr
+import pytest
+import os
+
+
+@pytest.mark.parametrize(
+    "whisper_type,vad_filter,bgm_separation,diarization",
+    [
+        ("whisper", True, False, False),
+        ("faster-whisper", True, False, False),
+        ("insanely_fast_whisper", True, False, False)
+    ]
+)
+def test_vad_pipeline(
+    whisper_type: str,
+    vad_filter: bool,
+    bgm_separation: bool,
+):
+    test_transcribe(whisper_type, vad_filter, bgm_separation)
