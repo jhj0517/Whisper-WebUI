29528210d45bd30c6e9b34d14a00c6d6027909c5
jhj0517 <97279763+jhj0517@users.noreply.github.com>
2024-10-01 17:04:30 +0900

    Use smallest model

--- tests/test_config.py
+++ tests/test_config.py
@@ -4,6 +4,6 @@ TEST_FILE_DOWNLOAD_URL = "https://github.com/jhj0517/whisper_flutter_new/raw/mai
 TEST_YOUTUBE_URL = "https://www.youtube.com/watch?v=4WEQtgnBu0I&ab_channel=AndriaFitzer"
 TEST_WHISPER_MODEL = "tiny"
 TEST_UVR_MODEL = "UVR-MDX-NET-Inst_HQ_4"
-TEST_NLLB_MODEL = "facebook/nllb-200-1.3B"
+TEST_NLLB_MODEL = "facebook/nllb-200-distilled-600M"
 TEST_SUBTITLE_PATH = os.path.join("translation_test.srt")
 
