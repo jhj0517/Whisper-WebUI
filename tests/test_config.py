7e7c0772d4c2a49044da36aa46d92d732c875b60
jhj0517 <97279763+jhj0517@users.noreply.github.com>
2024-10-01 17:30:49 +0900

    Add test subtitle files

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,3 +1,5 @@
+from modules.utils.paths import *
+
 import os
 
 TEST_FILE_DOWNLOAD_URL = "https://github.com/jhj0517/whisper_flutter_new/raw/main/example/assets/jfk.wav"
@@ -5,5 +7,6 @@ TEST_YOUTUBE_URL = "https://www.youtube.com/watch?v=4WEQtgnBu0I&ab_channel=Andri
 TEST_WHISPER_MODEL = "tiny"
 TEST_UVR_MODEL = "UVR-MDX-NET-Inst_HQ_4"
 TEST_NLLB_MODEL = "facebook/nllb-200-distilled-600M"
-TEST_SUBTITLE_PATH = os.path.join("translation_test.srt")
+TEST_SUBTITLE_SRT_PATH = os.path.join(WEBUI_DIR, "tests", "test_srt.srt")
+TEST_SUBTITLE_VTT_PATH = os.path.join(WEBUI_DIR, "tests", "test_vtt.vtt")
 
