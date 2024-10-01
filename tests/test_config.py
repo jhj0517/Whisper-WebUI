a17a92400340cab16ce4e3d079f566e1d481b6a2
jhj0517 <97279763+jhj0517@users.noreply.github.com>
2024-10-01 21:37:28 +0900

    Add test file path

--- tests/test_config.py
+++ tests/test_config.py
@@ -3,6 +3,7 @@ from modules.utils.paths import *
 import os
 
 TEST_FILE_DOWNLOAD_URL = "https://github.com/jhj0517/whisper_flutter_new/raw/main/example/assets/jfk.wav"
+TEST_FILE_PATH = os.path.join(WEBUI_DIR, "tests", "jfk.wav")
 TEST_YOUTUBE_URL = "https://www.youtube.com/watch?v=4WEQtgnBu0I&ab_channel=AndriaFitzer"
 TEST_WHISPER_MODEL = "tiny"
 TEST_UVR_MODEL = "UVR-MDX-NET-Inst_HQ_4"
