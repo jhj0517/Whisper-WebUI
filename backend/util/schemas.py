91f080095a63c6377f38175534eb076cf4d730db
jhj0517 <97279763+jhj0517@users.noreply.github.com>
2024-11-16 00:05:41 +0900

    Add task scheme

--- backend/util/schemas.py
+++ backend/util/schemas.py
@@ -1,5 +1,17 @@
 from pydantic import BaseModel, Field, validator
+from typing import List
 
 
 class QueueResponse(BaseModel):
+    identifier: str
     message: str
+
+
+class QueueTask(BaseModel):
+    identifier: str
+    status: str
+    task_type: str
+
+
+class QueueTasksResult(BaseModel):
+    tasks: List[QueueTask]
