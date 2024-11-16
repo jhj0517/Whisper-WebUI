d553981f8c1d21ef33286c0419ab3baf095b5273
jhj0517 <97279763+jhj0517@users.noreply.github.com>
2024-11-16 23:02:35 +0900

    Add functools and dotenv function

--- backend/util/config_loader.py
+++ backend/util/config_loader.py
@@ -1,8 +1,23 @@
-from modules.utils.paths import SERVER_CONFIG_PATH
+from dotenv import load_dotenv
+import os
+from modules.utils.paths import SERVER_CONFIG_PATH, SERVER_DOTENV_PATH
 from modules.utils.files_manager import load_yaml
 
+import functools
 
+
+@functools.lru_cache
 def load_server_config(config_path: str = SERVER_CONFIG_PATH) -> dict:
     return load_yaml(config_path)
 
+
+@functools.lru_cache
+def read_env(key: str, dotenv_path: str = SERVER_DOTENV_PATH):
+    load_dotenv(dotenv_path)
+    value = os.getenv(key)
+
+    if value is None:
+        raise KeyError(f"Key {key} does not exist in dotenv")
+
+    return value
 
