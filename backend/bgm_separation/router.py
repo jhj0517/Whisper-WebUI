1b0122c0c8ac84a11bf03a20baffd77b403ce344
jhj0517 <97279763+jhj0517@users.noreply.github.com>
2024-11-17 21:50:01 +0900

    Update to queue system

--- backend/bgm_separation/router.py
+++ backend/bgm_separation/router.py
@@ -7,15 +7,19 @@ from fastapi import (
 import gradio as gr
 from fastapi import APIRouter, BackgroundTasks, Depends, Response, status
 from typing import List, Dict, Tuple
+from datetime import datetime
 
 from modules.whisper.data_classes import *
 from modules.uvr.music_separator import MusicSeparator
 from ..common.audio import read_audio
 from ..common.models import QueueResponse
 from ..common.config_loader import load_server_config
+from ..db.task.models import TaskStatus, TaskType, ResultType
+from ..db.task.dao import add_task_to_db, update_task_status_in_db
+from .models import BGMSeparationResult
 
 
-bgm_separation_router = APIRouter(prefix="bgm-separation", tags=["BGM Separation"])
+bgm_separation_router = APIRouter(prefix="/bgm-separation", tags=["BGM Separation"])
 
 
 @functools.lru_cache
@@ -31,8 +35,18 @@ def get_bgm_separation_inferencer() -> 'MusicSeparator':
 
 async def run_bgm_separation(
     audio: np.ndarray,
-    params: BGMSeparationParams
+    params: BGMSeparationParams,
+    identifier: str,
 ) -> Tuple[np.ndarray, np.ndarray]:
+    update_task_status_in_db(
+        identifier=identifier,
+        update_data={
+            "id": identifier,
+            "status": TaskStatus.IN_PROGRESS,
+            "updated_at": datetime.utcnow()
+        }
+    )
+
     instrumental, vocal, filepaths = get_bgm_separation_inferencer().separate(
         audio=audio,
         model_name=params.model_size,
@@ -40,6 +54,21 @@ async def run_bgm_separation(
         segment_size=params.segment_size,
         save_file=False,
         progress=gr.Progress()
+    )
+    instrumental_path, vocal_path = filepaths
+
+    update_task_status_in_db(
+        identifier=identifier,
+        update_data={
+            "id": identifier,
+            "status": TaskStatus.COMPLETED,
+            "result": BGMSeparationResult(
+                instrumental_path=instrumental_path,
+                vocal_path=vocal_path
+            ),
+            "result_type": ResultType.FILEPATH,
+            "updated_at": datetime.utcnow()
+        }
     )
     return instrumental, vocal
 
@@ -57,11 +86,24 @@ async def bgm_separation(
     params: BGMSeparationParams = Depends()
 ) -> QueueResponse:
     if not isinstance(file, np.ndarray):
-        audio = await read_audio(file=file)
+        audio, info = await read_audio(file=file)
     else:
-        audio = file
+        audio, info = file, None
 
-    background_tasks.add_task(run_bgm_separation, audio=audio, params=params)
+    identifier = add_task_to_db(
+        status=TaskStatus.QUEUED,
+        file_name=file.filename,
+        audio_duration=info.duration if info else None,
+        task_type=TaskType.BGM_SEPARATION,
+        task_params=params.model_dump(),
+    )
+
+    background_tasks.add_task(
+        run_bgm_separation,
+        audio=audio,
+        params=params,
+        identifier=identifier
+    )
 
     return QueueResponse(message="Transcription task queued")
 
