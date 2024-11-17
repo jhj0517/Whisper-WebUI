11a8e2d327d95f8d8e73fc123fe3b0bd9c997b6a
jhj0517 <97279763+jhj0517@users.noreply.github.com>
2024-11-17 15:34:04 +0900

    Add common models

--- backend/common/models.py
+++ backend/common/models.py
@@ -1,9 +1,31 @@
 from pydantic import BaseModel, Field, validator
-from typing import List
+from typing import List, Any, Optional
+from ..db.task.models import TaskStatus
 
 
 class QueueResponse(BaseModel):
     identifier: str
+    status: TaskStatus
     message: str
 
 
+class Response(BaseModel):
+    identifier: str
+    message: str
+
+
+class Metadata(BaseModel):
+    task_type: str
+    task_params: Optional[dict]
+    language: Optional[str]
+    file_name: Optional[str]
+    url: Optional[str]
+    duration: Optional[float]
+    audio_duration: Optional[float] = None
+
+
+class Result(BaseModel):
+    status: str
+    result: Any
+    metadata: Metadata
+    error: Optional[str]
