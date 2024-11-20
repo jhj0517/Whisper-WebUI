4cf4155636f9ed8d86ef345180e670646514488b
jhj0517 <97279763+jhj0517@users.noreply.github.com>
2024-11-21 00:33:13 +0900

    Examine WER

--- backend/tests/test_backend_transcription.py
+++ backend/tests/test_backend_transcription.py
@@ -4,7 +4,10 @@ from io import BytesIO
 
 from backend.db.task.models import TaskStatus
 from backend.tests.test_task_status import wait_for_task_completion
-from backend.tests.test_backend_config import get_client, setup_test_file, get_upload_file_instance, TEST_PIPELINE_PARAMS
+from backend.tests.test_backend_config import (
+    get_client, setup_test_file, get_upload_file_instance, calculate_wer,
+    TEST_PIPELINE_PARAMS, TEST_ANSWER
+)
 
 
 @pytest.mark.parametrize(
@@ -41,4 +44,7 @@ def test_transcription_endpoint(
                                        f"expected time."
 
     result = completed_task.json()["result"]
+    assert result, "Transcription text is empty"
+    wer = calculate_wer(TEST_ANSWER, result[0]["text"].strip().replace(",", "").replace(".", ""))
+    assert wer < 0.1, f"WER is too high, it's {wer}"
 
