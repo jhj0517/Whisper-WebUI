8eb1da98cc39e6c00b5c0bceaf949989df1cfd2a
jhj0517 <97279763+jhj0517@users.noreply.github.com>
2024-11-21 02:28:24 +0900

    Add hash file functions

--- backend/common/compresser.py
+++ backend/common/compresser.py
@@ -1,6 +1,7 @@
 import os
 import zipfile
-from typing import List
+from typing import List, Optional
+import hashlib
 
 
 def compress_files(file_paths: List[str], output_zip_path: str) -> str:
@@ -25,3 +26,33 @@ def compress_files(file_paths: List[str], output_zip_path: str) -> str:
             file_name = os.path.basename(file_path)
             zipf.write(file_path, file_name)
     return output_zip_path
+
+
+def get_file_hash(file_path: str) -> str:
+    """Generate the hash of a file using the specified hashing algorithm. It generates hash by content not path. """
+    hash_func = hashlib.new("sha256")
+    try:
+        with open(file_path, 'rb') as f:
+            for chunk in iter(lambda: f.read(4096), b""):
+                hash_func.update(chunk)
+        return hash_func.hexdigest()
+    except FileNotFoundError:
+        return f"File not found: {file_path}"
+    except Exception as e:
+        return f"An error occurred: {str(e)}"
+
+
+def find_file_by_hash(dir_path: str, password: str) -> Optional[str]:
+    """Get file path from the directory based on its hash"""
+    if not os.path.exists(dir_path) and os.path.isdir(dir_path):
+        raise ValueError(f"Directory {dir_path} does not exist")
+
+    files = [os.path.join(dir_path, f) for f in os.listdir(dir_path) if os.path.isfile(os.path.join(dir_path, f))]
+
+    for f in files:
+        f_hash = get_file_hash(f)
+        if password == f_hash:
+            return f
+    return None
+
+
