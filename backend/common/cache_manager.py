8ca1b01e22d0bc62b323d31bd4c065f40b8e1160
jhj0517 <97279763+jhj0517@users.noreply.github.com>
2024-11-21 20:59:46 +0900

    Except placeholder

--- backend/common/cache_manager.py
+++ backend/common/cache_manager.py
@@ -7,8 +7,11 @@ from modules.utils.paths import BACKEND_CACHE_DIR
 
 def cleanup_old_files(cache_dir: str = BACKEND_CACHE_DIR, ttl: int = 60):
     now = time.time()
+    place_holder_name = "cached_files_are_generated_here"
     for root, dirs, files in os.walk(cache_dir):
         for filename in files:
+            if filename == place_holder_name:
+                continue
             filepath = os.path.join(root, filename)
             if now - os.path.getmtime(filepath) > ttl:
                 try:
