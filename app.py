d9ebe1e25b4ed507e6242f67ec8b5f76f7838496
jhj0517 <97279763+jhj0517@users.noreply.github.com>
2025-01-11 21:56:32 +0900

    Use the constant for `file_types`

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import yaml
 from modules.utils.paths import (FASTER_WHISPER_MODELS_DIR, DIARIZATION_MODELS_DIR, OUTPUT_DIR, WHISPER_MODELS_DIR,
                                  INSANELY_FAST_WHISPER_MODELS_DIR, NLLB_MODELS_DIR, DEFAULT_PARAMETERS_CONFIG_PATH,
                                  UVR_MODELS_DIR, I18N_YAML_PATH)
-from modules.utils.files_manager import load_yaml
+from modules.utils.files_manager import load_yaml, MEDIA_EXTENSION
 from modules.whisper.whisper_factory import WhisperFactory
 from modules.translation.nllb_inference import NLLBInference
 from modules.ui.htmls import *
@@ -95,20 +95,6 @@ class App:
         deepl_params = translation_params["deepl"]
         nllb_params = translation_params["nllb"]
         uvr_params = self.default_params["bgm_separation"]
-        
-        audio_extensions = [
-        '.mp3', '.wav', '.wma', '.aac', '.flac', '.ogg', 
-        '.m4a', '.aiff', '.alac', '.opus', '.webm', 
-        '.ac3', '.amr', '.au', '.mid', '.midi', '.mka'
-        ]
-
-        video_extensions = [
-            '.mp4', '.avi', '.mkv', '.mov', '.wmv', '.flv', 
-            '.webm', '.m4v', '.mpeg', '.mpg', '.3gp', 
-            '.ogv', '.ts', '.m2ts', '.divx', '.rmvb'
-        ]
-        
-        all_media_extensions = audio_extensions + video_extensions
 
         with self.app:
             lang = gr.Radio(choices=list(self.i18n.keys()),
@@ -122,7 +108,7 @@ class App:
                 with gr.Tabs():
                     with gr.TabItem(_("File")):  # tab1
                         with gr.Column():
-                            input_file = gr.Files(type="filepath", label=_("Upload File here"), file_types=all_media_extensions)
+                            input_file = gr.Files(type="filepath", label=_("Upload File here"), file_types=MEDIA_EXTENSION)
                             tb_input_folder = gr.Textbox(label="Input Folder Path (Optional)",
                                                          info="Optional: Specify the folder path where the input files are located, if you prefer to use local files instead of uploading them."
                                                               " Leave this field empty if you do not wish to use a local path.",
